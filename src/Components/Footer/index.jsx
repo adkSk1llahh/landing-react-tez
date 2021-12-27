21df6ebedf92b08b9a13363eac6aae464669c053
azhumabekov <azhumabekov@wooppay.com>
2021-12-27 17:53:43 +0600

    third commit

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -6,20 +6,20 @@ import wplogo from "../../assets/images/icons/wooppay-logo.svg";
 const Footer = () => {
   const { dictionary } = useContext(LanguageContext);
   return (
-    <footer class="footer container block">
-      <div class="footer__content">
+    <footer className="footer container block">
+      <div className="footer__content">
         <img
-          class="footer__logo"
+          className="footer__logo"
           src={logo}
           alt="image"
           width="32"
           height="32"
         />
-        <div class="footer__items">
-          <p class="footer__text footer__text--item">
+        <div className="footer__items">
+          <p className="footer__text footer__text--item">
             {dictionary.footer.text}
           </p>
-          <a class="footer__text footer__text--link" href="#">
+          <a className="footer__text footer__text--link" href="#">
             {dictionary.footer.link}
           </a>
         </div>
@@ -27,7 +27,7 @@ const Footer = () => {
 
       <a href="#">
         <img
-          class="footer__wp-logo"
+          className="footer__wp-logo"
           src={wplogo}
           alt="image"
           width="100"
